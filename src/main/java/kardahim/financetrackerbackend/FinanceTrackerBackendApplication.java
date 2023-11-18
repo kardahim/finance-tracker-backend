7aca2be96c3ab863dc31d74c9d250f8cb4212597
Paweł Ciszewski <kardahim@gmail.com>
2023-11-18 15:36:02 +0100

    init

---
+++ src/main/java/kardahim/financetrackerbackend/FinanceTrackerBackendApplication.java
@@ -0,0 +1,13 @@
+package kardahim.financetrackerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceTrackerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinanceTrackerBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kardahim/financetrackerbackend/FinanceTrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package kardahim.financetrackerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceTrackerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
