c9ba7d9ca44c39f6dc0315f8cd96aec4f0e6ead7
Paweł Ciszewski <kardahim@gmail.com>
2023-11-29 13:37:01 +0100

    change exp time of token

--- src/main/java/kardahim/financetrackerbackend/services/Impl/JWTServiceImpl.java
+++ src/main/java/kardahim/financetrackerbackend/services/Impl/JWTServiceImpl.java
@@ -19,7 +19,7 @@ public class JWTServiceImpl implements JWTService {
     public String generateToken(UserDetails userDetails) {
         return Jwts.builder().setSubject(userDetails.getUsername())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24))
                 .signWith(getSiginKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
@@ -27,7 +27,7 @@ public class JWTServiceImpl implements JWTService {
     public String generateRefreshToken(Map<String, Object> extraClaims, UserDetails userDetails) {
         return Jwts.builder().setClaims(extraClaims).setSubject(userDetails.getUsername())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 604800000))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24 * 7))
                 .signWith(getSiginKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
